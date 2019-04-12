a70c02873c3d53a303d373d1f1c062d692c97626
Radoslav Ognyanov <rognyanov@csc.com>
2019-04-12 15:11:35 +0300

    Initial commit

---
+++ app/src/main/java/com/teamapple/alzwit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.teamapple.alzwit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
