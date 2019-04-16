2f3c4da511e3feb91687af6f1b9e942ac7f898d3
snedelcheva2 <snedelcheva2@csc.com>
2019-04-16 14:32:13 +0300

    edit PassportActivity

--- app/src/main/java/com/teamapple/firebase/FirebaseMethods.java
+++ app/src/main/java/com/teamapple/firebase/FirebaseMethods.java
@@ -2,29 +2,37 @@ package com.teamapple.firebase;
 
 import android.content.Context;
 import android.util.Log;
+import android.support.annotation.NonNull;
 
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.teamapple.models.Notification;
 import com.teamapple.models.User;
 
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class FirebaseMethods {
+
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference myRef;
     private String userID;
-
+    User userData = new User();
     private Context mContext;
 
     public FirebaseMethods(Context context) {
         mAuth = FirebaseAuth.getInstance();
         mContext = context;
-        myRef = FirebaseDatabase.getInstance().getReference();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
 
         if (mAuth.getCurrentUser() != null) {
             userID = mAuth.getCurrentUser().getUid();
@@ -49,12 +57,28 @@ public class FirebaseMethods {
         dataToAdd.clear();
     }
 
-//    public User getCurrentUserData() {
-//        userID = mAuth.getCurrentUser().getUid();
-//        myRef = myRef.getDatabase().getReference("users/"+userID).addListenerForSingleValueEvent(){
-//
-//        };
-//    }
+    public User getCurrentUserData() {
+        userID = mAuth.getCurrentUser().getUid();
+
+        myRef.child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                    String key = dataSnapshot.getKey();
+                    if(userID.equals(key)){
+                        userData = data.getValue(User.class);
+
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        return userData;
+    }
 
     public void addNotification(Notification newNot) {
         userID = mAuth.getCurrentUser().getUid();
